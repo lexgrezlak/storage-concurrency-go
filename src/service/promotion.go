44011361ec7f4542ecab75e7a50194dbd31291cc
John <accs.x@pm.me>
2020-08-09 16:00:19 +0200

    Remove error channel from GetPromotionsFromCSV and format code

--- src/service/promotion.go
+++ src/service/promotion.go
@@ -10,11 +10,14 @@ import (
 )
 
 type Promotion struct {
-	Id string `json:"id"`
+	Id    string  `json:"id"`
 	Price float64 `json:"price"`
-	Date string `json:"date"`
+	Date  string  `json:"date"`
 }
 
+// First pulls a promotion from redis which is a json string,
+// and then transforms it into a Promotion interface,
+// and returns it.
 func (api *api) GetPromotionById(id string) (*Promotion, error) {
 	jsonStr, err := api.r.Get(context.Background(), id).Result()
 	if err != nil {
@@ -32,28 +35,22 @@ func (api *api) GetPromotionById(id string) (*Promotion, error) {
 
 func processRecordToPromotion(record []string) (*Promotion, error) {
 	id := record[0]
-
 	price, err := strconv.ParseFloat(record[1], 64)
 	if err != nil {
 		log.Printf("failed to parse float: %v", err)
 		return nil, err
 	}
-
 	date := record[2]
 
 	return &Promotion{id, price, date}, nil
 }
 
-
-func GetPromotionsFromCSV(ctx context.Context, b io.Reader) (<-chan *Promotion, <-chan error) {
+func GetPromotionsFromCSV(ctx context.Context, b io.Reader) <-chan *Promotion {
 	r := csv.NewReader(b)
-	concurrency := 1000
-	promotions := make(chan *Promotion, concurrency)
-	errs := make(chan error, concurrency)
+	promotions := make(chan *Promotion)
 
 	go func() {
 		defer close(promotions)
-		defer close(errs)
 		for {
 			select {
 			case <-ctx.Done():
@@ -63,18 +60,19 @@ func GetPromotionsFromCSV(ctx context.Context, b io.Reader) (<-chan *Promotion, 
 				if err == io.EOF {
 					break
 				} else if err != nil {
-					errs <- err
+					log.Printf("failed to read record from csv: %v", err)
+				} else {
+					p, err := processRecordToPromotion(record)
+					if err != nil {
+						log.Printf("failed to process record to promotion: %v", err)
+					} else {
+						promotions <- p
+					}
 				}
 
-				p, err := processRecordToPromotion(record)
-				if err != nil {
-					errs <- err
-				}
-
-				promotions <- p
 			}
 		}
 	}()
 
-	return promotions, errs
+	return promotions
 }
