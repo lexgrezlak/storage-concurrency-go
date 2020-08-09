24bbff35579aea44d7bf13e28190ab6fe46e461d
John <accs.x@pm.me>
2020-08-09 15:59:13 +0200

    Add charset=UTF-8 to header and close response body for optimization

--- src/handler/getPromotionById.go
+++ src/handler/getPromotionById.go
@@ -7,24 +7,31 @@ import (
 	"storage-api/src/service"
 )
 
+const (
+	ID = "id"
+)
+
 func GetPromotionById(datastore service.PromotionDatastore) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
 		vars := mux.Vars(r)
-		id := vars["id"]
+		id := vars[ID]
 
+		// Get the promotion (from redis)
 		promotion, err := datastore.GetPromotionById(id)
 		if err != nil {
 			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 			return
 		}
 
+		// Prepare the response data.
 		payload, err := json.Marshal(promotion)
 		if err != nil {
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
 
-		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write(payload)
 	}
